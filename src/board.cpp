e1cf29706a52e37db2b2411b26cecd18226a9ad3
Eirik Skogstad <eirik@echoforge.org>
2018-02-18 18:39:29 -0800

    Minor variable refactoring. Added EPD describe method. Added == overload for Position

--- src/board.cpp
+++ src/board.cpp
@@ -2,124 +2,129 @@
  * board.cpp
  *
  *  Created on: Dec 21, 2015
- *      Author: echo
+ *      Author: Eirik Skogstad
  */
 
 #include <cstdlib>
 #include <iostream>
-#include "pieces.h"
+#include <cmath>
+#include "piece.h"
 
-Position::Position(int row, int col) {
-	numberPosition = row;
-	letterPosition = col;
+Position::Position(int letter, int number) {
+	letter_ = letter;
+	number_ = number;
+}
+
+bool Position::operator==(const Position& other) const {
+	return letter() == other.letter() && 
+		   number() == other.number();
 }
 
 Position Position::stepNorth(int steps) {
-	return Position(numberPosition+steps, letterPosition);
+	return Position(letter_, number_+steps);
 }
 
 Position Position::stepSouth(int steps) {
-	return Position(numberPosition-steps, letterPosition);
+	return Position(letter_, number_-steps);
 }
 
 Position Position::stepEast(int steps) {
-	return Position(numberPosition, letterPosition+steps);
+	return Position(letter_+steps, number_);
 }
 
 Position Position::stepWest(int steps) {
-	return Position(numberPosition, letterPosition-steps);
+	return Position(letter_-steps, number_);
 }
 
 Position Position::stepNorthEast(int steps) {
-	return Position(numberPosition+steps, letterPosition+steps);
+	return Position(letter_+steps, number_+steps);
 }
 
 Position Position::stepNorthWest(int steps) {
-	return Position(numberPosition+steps, letterPosition-steps);
+	return Position(letter_-steps, number_+steps);
 }
 
 Position Position::stepSouthEast(int steps) {
-	return Position(numberPosition-steps, letterPosition+steps);
+	return Position(letter_+steps, number_-steps);
 }
 
 Position Position::stepSouthWest(int steps) {
-	return Position(numberPosition-steps, letterPosition-steps);
+	return Position(letter_-steps, number_-steps);
 }
 
 Board::Board() {
 
 	// Allocate memory for the board
-	board = new Piece**[height];
-	for (int i=0; i<width; i++)
-		board[i] = new Piece*[width];
-
+	board_ = new Piece**[height_];
+	for (int i=0; i<width_; i++)
+		board_[i] = new Piece*[width_];
 
 	// Initialize white pieces
-	board[0][0] = new Rook(WHITE, Position(0,0));
-	board[0][1] = new Knight(WHITE, Position(0,1));
-	board[0][2] = new Bishop(WHITE, Position(0,2));
-	board[0][3] = new Queen(WHITE, Position(0,3));
-	board[0][4] = new King(WHITE, Position(0,4));
-	board[0][5] = new Bishop(WHITE, Position(0,5));
-	board[0][6] = new Knight(WHITE, Position(0,6));
-	board[0][7] = new Rook(WHITE, Position(0,7));
-	board[1][0] = new Pawn(WHITE, Position(1,0));
-	board[1][1] = new Pawn(WHITE, Position(1,1));
-	board[1][2] = new Pawn(WHITE, Position(1,2));
-	board[1][3] = new Pawn(WHITE, Position(1,3));
-	board[1][4] = new Pawn(WHITE, Position(1,4));
-	board[1][5] = new Pawn(WHITE, Position(1,5));
-	board[1][6] = new Pawn(WHITE, Position(1,6));
-	board[1][7] = new Pawn(WHITE, Position(1,7));
+    board_[0][0] = new Rook    (WHITE, Position(0,0));
+    board_[0][1] = new Knight  (WHITE, Position(1,0));
+    board_[0][2] = new Bishop  (WHITE, Position(2,0));
+    board_[0][3] = new Queen   (WHITE, Position(3,0));
+    board_[0][4] = new King    (WHITE, Position(4,0));
+    board_[0][5] = new Bishop  (WHITE, Position(5,0));
+    board_[0][6] = new Knight  (WHITE, Position(6,0));
+    board_[0][7] = new Rook    (WHITE, Position(7,0));
+    board_[1][0] = new Pawn    (WHITE, Position(0,1));
+    board_[1][1] = new Pawn    (WHITE, Position(1,1));
+    board_[1][2] = new Pawn    (WHITE, Position(2,1));
+    board_[1][3] = new Pawn    (WHITE, Position(3,1));
+    board_[1][4] = new Pawn    (WHITE, Position(4,1));
+    board_[1][5] = new Pawn    (WHITE, Position(5,1));
+    board_[1][6] = new Pawn    (WHITE, Position(6,1));
+    board_[1][7] = new Pawn    (WHITE, Position(7,1));
 
-	// Initialize black pieces
-	board[7][0] = new Rook(BLACK, Position(7,0));
-	board[7][1] = new Knight(BLACK, Position(7,1));
-	board[7][2] = new Bishop(BLACK, Position(7,2));
-	board[7][3] = new Queen(BLACK, Position(7,3));
-	board[7][4] = new King(BLACK, Position(7,4));
-	board[7][5] = new Bishop(BLACK, Position(7,5));
-	board[7][6] = new Knight(BLACK, Position(7,6));
-	board[7][7] = new Rook(BLACK, Position(7,7));
-	board[6][0] = new Pawn(BLACK, Position(6,0));
-	board[6][1] = new Pawn(BLACK, Position(6,1));
-	board[6][2] = new Pawn(BLACK, Position(6,2));
-	board[6][3] = new Pawn(BLACK, Position(6,3));
-	board[6][4] = new Pawn(BLACK, Position(6,4));
-	board[6][5] = new Pawn(BLACK, Position(6,5));
-	board[6][6] = new Pawn(BLACK, Position(6,6));
-	board[6][7] = new Pawn(BLACK, Position(6,7));
+    // Initialize black pieces
+    board_[7][0] = new Rook    (BLACK, Position(0,7));
+    board_[7][1] = new Knight  (BLACK, Position(1,7));
+    board_[7][2] = new Bishop  (BLACK, Position(2,7));
+    board_[7][3] = new Queen   (BLACK, Position(3,7));
+    board_[7][4] = new King    (BLACK, Position(4,7));
+    board_[7][5] = new Bishop  (BLACK, Position(5,7));
+    board_[7][6] = new Knight  (BLACK, Position(6,7));
+    board_[7][7] = new Rook    (BLACK, Position(7,7));
+    board_[6][0] = new Pawn    (BLACK, Position(0,6));
+    board_[6][1] = new Pawn    (BLACK, Position(1,6));
+    board_[6][2] = new Pawn    (BLACK, Position(2,6));
+    board_[6][3] = new Pawn    (BLACK, Position(3,6));
+    board_[6][4] = new Pawn    (BLACK, Position(4,6));
+    board_[6][5] = new Pawn    (BLACK, Position(5,6));
+    board_[6][6] = new Pawn    (BLACK, Position(6,6));
+    board_[6][7] = new Pawn    (BLACK, Position(7,6));
 }
 
 void Board::clear() {
-	for (int i = 0; i < height; i++) {
-		for (int j = 0; j < width; j++) {
-			board[i][j] = NULL;
+	for (int i = 0; i < height_; i++) {
+		for (int j = 0; j < width_; j++) {
+			board_[i][j] = NULL;
 		}
 	}
 }
 
-std::vector<Piece*> Board::get() {
+std::vector<Piece*> Board::pieces() {
 	std::vector<Piece*> pieces;
-	for (int i = 0; i < height; i++) {
-		for (int j = 0; j < width; j++) {
-			if (board[i][j] != NULL)
-				pieces.push_back(board[i][j]);
+	for (int i = 0; i < height_; i++) {
+		for (int j = 0; j < width_; j++) {
+			if (board_[i][j] != NULL)
+				pieces.push_back(board_[i][j]);
 		}
 	}
 
 	return pieces;
 }
 
-void Board::set(std::vector<Piece*> pieces) {
+void Board::placePieces(std::vector<Piece*> pieces) {
 	for(std::vector<Piece*>::iterator it = pieces.begin(); it != pieces.end(); ++it) {
 		Piece *p = *it;
-		board[p->getRow()][p->getCol()] = p;
+		board_[p->position().number()][p->position().letter()] = p;
 	}
 }
 
-void Board::setSquare(Position pos, Piece* piece) {
-	board[pos.getNumber()][pos.getLetter()] = piece;
+void Board::placePiece(Position pos, Piece* piece) {
+	board_[pos.number()][pos.letter()] = piece;
 }
 
 int Board::evaluate() {
@@ -128,16 +133,16 @@ int Board::evaluate() {
 	int whiteSquarePoints = 0;
 	int blackSquarePoints = 0;
 
-	for (int i=0; i<height; i++) {
-		for (int j=0; j<width; j++) {
-			Piece *piece = board[i][j];
+	for (int i=0; i<height_; i++) {
+		for (int j=0; j<width_; j++) {
+			Piece *piece = board_[i][j];
 			if (piece != NULL) {
 				if (piece->getSide() == WHITE) {
 					whitePiecePoints += piece->getValue();
-					whiteSquarePoints += piece->getPosition().getValue();
+					whiteSquarePoints += piece->position().value();
 				} else {
 					blackPiecePoints += piece->getValue();
-					blackSquarePoints += piece->getPosition().getValue();
+					blackSquarePoints += piece->position().value();
 				}
 			}
 		}
@@ -147,76 +152,134 @@ int Board::evaluate() {
 }
 
 bool Board::isOutOfBounds(Position position) {
-	bool a = position.getNumber() < height;
-	bool b = position.getLetter() < width;
-	bool c = position.getNumber() >= 0;
-	bool d = position.getLetter() >= 0;
+	bool a = position.number() < height_;
+	bool b = position.letter() < width_;
+	bool c = position.number() >= 0;
+	bool d = position.letter() >= 0;
 	return !( a && b && c && d );
 }
 
-std::vector<Piece*> Board::getPiecesBySide(Side side) {
+std::vector<Piece*> Board::pieces(Side side) {
 
 	std::vector<Piece*> pieces;
-	for (int i=0; i<height; i++) {
-			for (int j=0; j<width; j++) {
-				Piece *p = board[i][j];
+	for (int i=0; i<height_; i++) {
+			for (int j=0; j<width_; j++) {
+				Piece *p = board_[i][j];
 				if (p != NULL && p->getSide() == side)
-					pieces.push_back(board[i][j]);
+					pieces.push_back(board_[i][j]);
 			}
 	}
 
 	return pieces;
 }
 
-std::vector<Piece*> Board::getWhitePieces() {
-	return getPiecesBySide(WHITE);
+std::vector<Piece*> Board::whitePieces() {
+	return pieces(WHITE);
 }
 
-std::vector<Piece*> Board::getBlackPieces() {
-	return getPiecesBySide(BLACK);
+std::vector<Piece*> Board::blackPieces() {
+	return pieces(BLACK);
 }
 
-Piece *Board::getPieceAt(Position position) {
+Piece *Board::pieceAt(Position position) {
 	if (!isOutOfBounds(position))
-		return board[position.getNumber()][position.getLetter()];
+		return board_[position.number()][position.letter()];
 
 	return NULL;
 }
 
 
 void Board::capturePiece(Position position) {
-	Piece *piece = getPieceAt(position);
-	board[position.getNumber()][position.getLetter()] = NULL;
+	board_[position.number()][position.letter()] = NULL;
 }
 
 void Board::movePiece(Position from, Position to) {
-	Piece *moved = board[from.getNumber()][from.getLetter()];
-	board[from.getNumber()][from.getLetter()] = NULL;
-	board[to.getNumber()][to.getLetter()] = moved;
+    // TODO: Check outofbounds
+	Piece *moved = board_[from.number()][from.letter()];
+	board_[from.number()][from.letter()] = NULL;
+	board_[to.number()][to.letter()] = moved;
+
+	next_to_move_ = next_to_move_ == WHITE ? BLACK : WHITE;
+}
+
+char* Board::Describe() {
+    char* epd_string = new char[128];
+    int string_position = 0;
+    char separator = '/';
+
+    // Create representation of piece positions
+    for (int rank = BLACK_OFFICER_ROW; rank >= WHITE_OFFICER_ROW; rank--) {
+        int blank_squares = 0;
+        for (int file=0; file<width_; file++) {
+            Piece* piece = board_[rank][file];
+
+            if (piece != NULL && blank_squares > 0) {
+                epd_string[string_position++] = blank_squares+'0';
+                blank_squares = 0;
+                //epd_string[string_position++] = separator;
+            } else if (piece == NULL) {
+                blank_squares++;
+                continue;
+            }
+
+            epd_string[string_position++] = piece->toChar();
+            //epd_string[string_position++] = separator;
+        }
+
+        if (blank_squares > 0) {
+            epd_string[string_position++] = blank_squares+'0';
+            blank_squares = 0;
+        }
+
+        epd_string[string_position++] = separator;
+    }
+
+    epd_string[string_position-1] = ' ';
+
+    epd_string[string_position++] = next_to_move_ == WHITE ? 'w' : 'b';
+    epd_string[string_position++] = ' ';
+
+    // Create the castling string using a bitmask because it's cool
+    char castling[4] = {'K', 'Q', 'k', 'q'};
+    unsigned int a = black_can_castle_queen << 3 | black_can_castle_king << 2 | white_can_castle_queen << 1 | white_can_castle_king;
+
+    for (int i = 0; i<4; i++) {
+        epd_string[string_position] = char (int (castling[i]) * (a&(unsigned int) pow(2,i))>>i);
+        if (int (epd_string[string_position]) != 0)
+            string_position++;
+    }
+
+    // Terminate and return the string
+    epd_string[string_position++] = ' ';
+    epd_string[string_position++] = '-';
+    epd_string[string_position] = '\0';
+    return epd_string;
 }
 
 void Board::draw() {
 
 	std::cout << "   ";
-	for (int j=0; j<width*4-1; j++) {
+	for (int j=0; j<width_*4-1; j++) {
 		std::cout << "-";
 	}
 	std::cout << std::endl;
 
-	for (int i=height-1; i>=0; i--) {
+	for (int i=height_-1; i>=0; i--) {
 		std::cout << i+1 << " | ";
-		for (int j=0; j<width; j++) {
-			Piece* p = board[i][j];
+		for (int j=0; j<width_; j++) {
+			Piece* p = board_[i][j];
 			if (p == NULL)
 				std::cout << "  | ";
 			else
 				std::cout << p->toChar() << " " << "|" << " ";
 		}
 		std::cout << std::endl << "   ";
-		for (int j=0; j<width*4-1; j++) {
+		for (int j=0; j<width_*4-1; j++) {
 			std::cout << "-";
 		}
 		std::cout << std::endl;
 	}
 	std::cout << "    A   B   C   D   E   F   G   H" << std::endl;
+
+	std::cout << "\nThe EPD representation if this board is " << Describe() << std::endl;
 }
--- src/board.h
+++ src/board.h
@@ -2,7 +2,7 @@
  * board.h
  *
  *  Created on: Dec 21, 2015
- *      Author: echo
+ *      Author: Eirik Skogstad
  */
 
 #ifndef BOARD_H_
@@ -12,71 +12,98 @@
 
 class Piece;
 
-const int WHITE_OFFICER_ROW = 0;
-const int WHITE_PAWN_ROW = 1;
-const int WHITE_ENPASSANT_ROW = 5;
-
-const int BLACK_OFFICER_ROW = 7;
-const int BLACK_PAWN_ROW = 6;
-const int BLACK_ENPASSANT_ROW = 4;
+const int WHITE_OFFICER_ROW     =   0;
+const int WHITE_PAWN_ROW        =   1;
+const int WHITE_ENPASSANT_ROW   =   5;
+const int BLACK_OFFICER_ROW     =   7;
+const int BLACK_PAWN_ROW        =   6;
+const int BLACK_ENPASSANT_ROW   =   4;
 
 class Position {
-private:
-	int numberPosition;
-	int letterPosition;
+  public:
+    Position() : letter_(0), number_(0) {}
+    Position(int letter, int number);
 
-public:
+    bool operator==(const Position& other) const;
 
-	Position() : numberPosition(0), letterPosition(0) {}
-	Position(int row, int col);
+    int letter() const { return letter_; }
+    int number() const { return number_; }
+    int value()  const { if (number_ <= 4 && number_ >= 3) return 10; return 0; }
 
-	int getNumber() {return numberPosition;}
-	int getLetter() {return letterPosition;}
-	int getValue()  { if (numberPosition <= 4 && numberPosition >= 3) return 10; return 0; }
+    // Functions for taking n=steps steps in a given direction.
+    // myPosition.stepNorth() will return the position at one step north of
+    // myPosition.
+    Position stepNorth     (int steps = 1);
+    Position stepNorthEast (int steps = 1);
+    Position stepNorthWest (int steps = 1);
+    Position stepSouth     (int steps = 1);
+    Position stepSouthEast (int steps = 1);
+    Position stepSouthWest (int steps = 1);
+    Position stepEast      (int steps = 1);
+    Position stepWest      (int steps = 1);
 
-	Position stepNorth(int steps=1);
-	Position stepNorthEast(int steps=1);
-	Position stepNorthWest(int steps=1);
-	Position stepSouth(int steps=1);
-	Position stepSouthEast(int steps=1);
-	Position stepSouthWest(int steps=1);
-	Position stepEast(int steps=1);
-	Position stepWest(int steps=1);
+  private:
+    int letter_;
+    int number_;
 };
 
 class Board {
-private:
-	static const int height = 8;
-	static const int width = 8;
-	Piece ***board;
+    friend class Piece;
+    friend class King;
+    friend class Queen;
+    friend class Bishop;
+    friend class Knight;
+    friend class Rook;
+    friend class Pawn;
 
-public:
-	Board();
+  public:
+    Board();
 
-	void setBoard(Piece ***newBoard) {board = newBoard;}
+    // Functions for placing and removing pieces on the board
+    void placeAllPieces(Piece ***newBoard) { board_ = newBoard; }
+    void placePieces(std::vector<Piece*> pieces);
+    void placePiece(Position pos, Piece* piece);
+    void clear();
 
-	std::vector<Piece*> get();
-	void set(std::vector<Piece*> pieces);
-	void setSquare(Position pos, Piece* piece);
-	void clear();
+    // Functions for extracting pointers to pieces on the board
+    Piece *pieceAt(Position position);
+    std::vector<Piece*> pieces();
+    std::vector<Piece*> pieces(Side side);
+    std::vector<Piece*> whitePieces();
+    std::vector<Piece*> blackPieces();
 
-	int evaluate();
+    // Evaluate the current advantage for the white player.
+    // In chess terms, this is the evaluation of the position boiled down to a
+    // single numeric value.
+    // A higher value signifies more advantage for white and vice versa.
+    int evaluate();
 
-	int getHeight(){return height;}
-	int getWidth(){return width;}
-	bool isOutOfBounds(Position position);
+    void capturePiece(Position position);
+    void movePiece(Position from, Position to);
 
-	std::vector<Piece*> getPiecesBySide(Side side);
-	std::vector<Piece*> getWhitePieces();
-	std::vector<Piece*> getBlackPieces();
-	Piece *getPieceAt(Position position);
+    Position *getEnPassantPosition() { return 0; }
 
-	void capturePiece(Position position);
-	void movePiece(Position from, Position to);
+    // Return a string representation of the current position
+    char* Describe();
 
-	void draw();
+    // TODO: This has no business here, should be moved to a Graphics/Console class.
+    void draw();
+
+  private:
+    static const int height_ = 8;
+    static const int width_  = 8;
+    Piece ***board_;
+
+    bool white_can_castle_king = true;
+    bool white_can_castle_queen = true;
+    bool black_can_castle_king = true;
+    bool black_can_castle_queen = true;
+
+    Side next_to_move_ = WHITE;
+
+    int height() { return height_; }
+    int width()  { return width_; }
+    bool isOutOfBounds(Position position);
 };
-
-
 
 #endif /* BOARD_H_ */
